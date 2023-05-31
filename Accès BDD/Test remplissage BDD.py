7ccb73bb40438e80f4272d61db6b9f81a05edaa0
Thomas PELTIER <123626866+Thomas17000@users.noreply.github.com>
2023-05-31 11:41:26 +0200

    Update Test remplissage BDD.py

--- Accès BDD/Test remplissage BDD.py
+++ Accès BDD/Test remplissage BDD.py
@@ -1,10 +1,5 @@
 import mysql.connector
-import paho.mqtt.client as mqtt
 
-# Paramètres de connexion au broker MQTT
-broker_address = "172.20.233.40"
-broker_port = 1883
-topic = "test_co2"
 
 # Paramètres de connexion à la base de données
 db_host = "172.20.233.40"
@@ -12,34 +7,10 @@ db_user = "phpmyadmin"
 db_password = "phpmyadmin"
 db_name = "Base_Projet"
 
-# Fonction de callback qui est appelée lorsque le client MQTT se connecte au broker
-def on_connect(client, userdata, flags, rc):
-    print("Connexion au broker MQTT établie avec succès")
 
-    # Abonnement au topic
-    client.subscribe(topic)
-
-
-# Fonction de callback qui est appelée lorsque le client MQTT reçoit un message
-def on_message(client, userdata, message):
-    print("Message reçu sur le topic :", message.topic)
-    print("Contenu du message :", message.payload.decode())
-
-
-# Création d'un client MQTT
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-
-# Connexion au broker MQTT
-client.connect(broker_address, broker_port)
 
 
 while 1 == 1:
-    client.loop_forever() 
-# Boucle principale du client MQTT
-
 
     # Connexion à la base de données
     cnx = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
