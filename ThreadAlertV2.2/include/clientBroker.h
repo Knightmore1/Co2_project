9668af9d291210fbf4024a95e5a56c00017d50f6
Kreoz42 <48868173+Kreoz42@users.noreply.github.com>
2023-06-14 16:17:07 +0200

    Add files via upload
    FINAL

---
+++ ThreadAlertV2.2/include/clientBroker.h
@@ -0,0 +1,18 @@
+#include <WiFi.h>        //Libs wifi et envoie de données
+#include <PubSubClient.h>
+
+// Définition de la classe MaClasse
+class clientBroker 
+{
+    public:
+        WiFiClient wifiClient;
+
+        void connexionWIFI(const char* ssid, const char* password);
+        
+        void connexionMQTT(const char* mqttServer, const int mqttPort, const char* mqttUser, const char* mqttPassword);
+        
+        void client(float humidity, float temperature, int co2);
+
+};
+
+void sendMacAddress();
